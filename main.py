2977c2b024ee13a93e0cceaa9f4d169ec8dd565c
mohsin651 <mohsinimam651@gmail.com>
2023-07-15 19:24:42 +0530

    initial commit

--- main.py
+++ main.py
@@ -1,6 +1,57 @@
 import streamlit as st
 import pandas as pd
-import openai_helper
+import openai
+import json
+import pandas as pd
+
+secret_key = "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"
+
+openai.api_key = secret_key
+
+def extract_financial_data(text):
+    prompt = get_prompt_financial() + text
+    response = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=[{"role": "system", "content": prompt}]
+    )
+    content = response['choices'][0]['message']['content']
+
+    try:
+        data = json.loads(content)
+        return pd.DataFrame(data.items(), columns=["Measure", "Value"])
+
+    except (json.JSONDecodeError, IndexError):
+        return None
+
+def get_prompt_financial():
+    return '''
+    Please retrieve company name, revenue, net income and earnings per share (a.k.a. EPS)
+    from the following news article. If you can't find the information from this article 
+    then return "". Do not make things up.    
+    Then retrieve a stock symbol corresponding to that company. For this you can use
+    your general knowledge (it doesn't have to be from this article). Always return your
+    response as a valid JSON string. The format of that string should be this, 
+    {
+        "Company Name": "Walmart",
+        "Stock Symbol": "WMT",
+        "Revenue": "12.34 million",
+        "Net Income": "34.78 million",
+        "EPS": "2.1 $"
+    }
+    News Article:
+    ============
+    '''
+
+if __name__ == '__main__':
+    text = '''
+    Tesla's Earning news in text format: Tesla's earning this quarter blew all the estimates. They reported 4.5 billion $ profit against a revenue of 30 billion $. Their earnings per share was 2.3 $
+    '''
+
+    df = extract_financial_data(text)
+    if df is not None:
+        print(df.to_string())
+    else:
+        print("No financial data extracted.")
 
 col1, col2 = st.columns([3,2])
 
@@ -13,7 +64,7 @@ with col1:
     st.title("Financial Data Extraction Tool")
     news_article = st.text_area("Paste your financial news article here", height=300)
     if st.button("Extract"):
-        financial_data_df = openai_helper.extract_financial_data(news_article)
+        financial_data_df = extract_financial_data(news_article)
 
 with col2:
     st.markdown("<br/>" * 5, unsafe_allow_html=True)  # Creates 5 lines of vertical space
